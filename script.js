a86a213cdf32f68e787d152ac427a8c090c6c4ff
Kara9125 <K.nicholas9125@gmail.com>
2015-07-05 22:57:20 -0700

    second commit

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ $(function(){
 	}
 	var postTemplate = _.template($('#post-template').html());
 	console.log(postTemplate)
-	var post1 = new Posts("Second Entry", "clean clothes");
-	var post2 = new Posts("First Blog Entry", "paint wall");
+	var post1 = new Posts("Second Entry", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed venenatis erat, nec suscipit orci. Integer facilisis consectetur turpis. Aenean congue tortor quis ante mattis vestibulum. Phasellus congue ultricies turpis, ut commodo ipsum facilisis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
+	var post2 = new Posts("First Blog Entry", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sed venenatis erat, nec suscipit orci. Integer facilisis consectetur turpis. Aenean congue tortor quis ante mattis vestibulum. Phasellus congue ultricies turpis, ut commodo ipsum facilisis eget. Lorem ipsum dolor sit amet, consectetur adipiscing elit.");
 
 	Posts.all = [post1, post2];
 	_.each(Posts.all, function (post, index) {
